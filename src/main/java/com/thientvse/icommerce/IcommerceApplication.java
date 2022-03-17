61e890567a896b4ba469b88d75c8d3e5e26e0599
Thientv7 <thientv7@viettel.com.vn>
2022-03-17 14:04:04 +0700

    Initial commit

---
+++ src/main/java/com/thientvse/icommerce/IcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.thientvse.icommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thientvse/icommerce/IcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thientvse.icommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
