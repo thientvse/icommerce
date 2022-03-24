34d01be808da709419132aecd01b89ecceb8db60
Thientv7 <thientv7@viettel.com.vn>
2022-03-24 18:08:18 +0700

    Fix sql

--- document/init.sql
+++ document/init.sql
@@ -1,3 +1,7 @@
+create database icommerce;
+
+use icommerce;
+
 create table brand
 (
     id   int auto_increment
@@ -13,6 +17,13 @@ create table category
 );
 
 create table color
+(
+    id   int auto_increment
+        primary key,
+    name varchar(50) null
+);
+
+create table option
 (
     id   int auto_increment
         primary key,
@@ -75,5 +86,35 @@ create table cart
 
 
 
+
+# brand
+INSERT INTO icommerce.brand (id, name)
+VALUES (1, 'Samsung');
+INSERT INTO icommerce.brand (id, name)
+VALUES (2, 'Apple');
+
+# category
+INSERT INTO icommerce.category (id, name)
+VALUES (1, 'Điện thoại');
+INSERT INTO icommerce.category (id, name)
+VALUES (2, 'Điện tử');
+
+# color
+INSERT INTO icommerce.color (id, name)
+VALUES (1, 'Red');
+INSERT INTO icommerce.color (id, name)
+VALUES (2, 'Blue');
+INSERT INTO icommerce.color (id, name)
+VALUES (3, 'Pink');
+
+# user
+INSERT INTO icommerce.customer (id, email, full_name, shipping_address, billing_address, phone_number)
+VALUES (1, 'thientvse@gmail.com', 'Thiiều Văn Thiện', 'Tây Mỗ, Nam Từ Liêm', 'Tây Mỗ, Nam Từ Liêm', '0389937014');
+
+
+#
+INSERT INTO icommerce.product (id, product_name, description, category_id, brand_id, color_id, quantity, unit_price) VALUES (1, 'Điện thoại Samsung', 'Một sản phẩm của Samsung', 1, 1, 1, 100, 1000);
+INSERT INTO icommerce.product (id, product_name, description, category_id, brand_id, color_id, quantity, unit_price) VALUES (2, 'Iphone 13', 'Một sản phẩm của Apple', 1, 2, 2, 50, 1000);
+INSERT INTO icommerce.product (id, product_name, description, category_id, brand_id, color_id, quantity, unit_price) VALUES (3, 'Tủ lạnh Samsung', 'Một sản phẩm của Samsung', 2, 1, 2, 50, 500);
 
 
